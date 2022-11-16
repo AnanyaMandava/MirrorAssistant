5f3e54f73ad30f5b6f7ab3dd84c498f7be9593f9
pragneshbagary <41118746+pragneshbagary@users.noreply.github.com>
2022-11-16 12:09:53 -0800

    Update detector_utils.py

--- backend/utils/detector_utils.py
+++ backend/utils/detector_utils.py
@@ -17,13 +17,13 @@ sys.path.append("..")
 
 MODEL_NAME = 'hand_inference_graph'
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
-PATH_TO_CKPT = '/Users/pragneshbagary/Downloads/majorproject/Mirror/backend/backend/hand_inference_graph/frozen_inference_graph.pb'
+PATH_TO_CKPT = 'backend/hand_inference_graph/frozen_inference_graph.pb'
 # List of the strings that is used to add correct label for each box.
 PATH_TO_LABELS = os.path.join(MODEL_NAME, 'hand_label_map.pbtxt')
 
 NUM_CLASSES = 1
 # load label map
-label_map = label_map_util.load_labelmap('/Users/pragneshbagary/Downloads/majorproject/Mirror/backend/backend/hand_inference_graph/hand_label_map.pbtxt')
+label_map = label_map_util.load_labelmap('backend/hand_inference_graph/hand_label_map.pbtxt')
 categories = label_map_util.convert_label_map_to_categories(
     label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
